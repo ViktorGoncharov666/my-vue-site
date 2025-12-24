94ad0c34fadd9db5167d2f8795f275ba1fbe6ed1
Vector <super000people@gmail.com>
2025-12-24 06:22:03 +0300

    412213

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import router from './router'
 import store from './store.js'
 
 import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import BootstrapVue3 from 'bootstrap-vue-3'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 import 'bootstrap/dist/js/bootstrap.js'
 
 const app = createApp(App)
